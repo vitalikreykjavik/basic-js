c59079ffe2b15d1f53431e33aff9391a9238559a
Vitali Serada <gomelska@gmail.com>
2020-03-22 23:43:35 +0300

    feat: try extended repeater tsak

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,31 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  //let add = options.addition;
+  //let addSep = options.additionSeparator;
+  if (typeof str !== 'string') {
+    str.toString();
+  } else if (typeof options.addition !== 'string') {
+    options.addition.toString();
+  }; 
+  
+  if ('separator' in options === false) {
+    options.separator = '+';
+    } else if ('additionSeparator' in options === false) {
+      options.additionSeparator = '|';
+    };
+
+  if ('repeatTimes' in options === false) {
+    options.repeatTimes = -1;
+    } else if ('additionRepeatTimes' in options === false) {
+      options.additionRepeatTimes = -1;
+    }; 
+  
+  let addTo = options.addition + options.additionSeparator;
+  let addToRepeated = addTo.repeat(options.additionRepeatTimes - 1) + options.addition;
+  let preBase = str + addToRepeated + options.separator;
+  let base = str + addToRepeated;
+  let almostResult = preBase.repeat(options.repeatTimes - 1);
+  let result = almostResult + base;
+
+  return result;
 };
   
