85cb5871cb0df295e6ca04f770656f88ebecd2c1
Vitali Serada <gomelska@gmail.com>
2020-03-22 23:49:26 +0300

    feat: try recursive calc task

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,6 +1,7 @@
 module.exports = class DepthCalculator {
-    calculateDepth(/* arr */) {
-        throw 'Not implemented';
-        // remove line with error and write your code here
-    }
+    calculateDepth(arr) {
+        if (Array.isArray(arr)) {
+            return 1;
+        };
+    };
 };
