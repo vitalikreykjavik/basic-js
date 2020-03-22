f3af6f4bf254f3c9134754207257294d19f78ec2
Vitali Serada <gomelska@gmail.com>
2020-03-22 20:30:41 +0300

    feat: add js tasks functions

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,23 +1,27 @@
 const chainMaker = {
+  result: [],
   getLength() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    return this.result.length;
   },
   addLink(value) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    this.result.push(value);  
+    return this;  
   },
   removeLink(position) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    if (position < this.result.length && position > 0 && isNaN(position) === false) {
+      this.result.splice(position - 1, 1);
+      return this;
+    } else {
+      throw new Error();
+    };
   },
   reverseChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    this.result.reverse();
+    return this;
   },
   finishChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    this.result = this.result.map(chainValue => `( ${chainValue} )`).join('~~');
+    return this.result;
   }
 };
 
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,30 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (Array.isArray(arr) !== true) {
+    throw new Error(); 
+  };
+
+  let result = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[0] === '--discard-prev' || arr[0] === '--double-prev') {
+      i++;
+    };
+    if (arr[arr.length - 1] === '--double-next' || arr[arr.length - 1] === '--discard-next') {
+      return result;
+    };
+    if (arr[i] === '--discard-next') {
+      i++;
+    } else if (arr[i] === '--double-next') {
+      result.push(arr[i + 1]);
+    } else if (arr[i] === '--discard-prev') {
+      result.splice(i - 1, 1);
+    } else if (arr[i] === '--double-prev') {
+      result.push(arr[i - 1]);
+    } else {
+      result.push(arr[i]);
+    };
+  };
+  return result;
 };
+
+
